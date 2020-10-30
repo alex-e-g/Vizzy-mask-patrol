267a6b7b0de306211f5c947f7e72ee7f3b5e0e10
Alexandra Gonçalves <57375258+alex-e-g@users.noreply.github.com>
2020-10-30 10:18:31 +0000

    Add files via upload

--- random_movement.py
+++ random_movement.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python 
+
+#Joao Avelino, 2020.
+#ISR-Lisboa / IST
+
+#A template of a GeneralActionlib actionserver for you to use
 
 import random
 import ast
@@ -31,6 +37,11 @@ class RandomMovementServer(object):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, GeneralAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
+
+        #Choosing the desired coordinates
+	self.txt=open("/home/alex/Desktop/coords.txt","r")
+	self.limits=ast.literal_eval(self.txt.read())
+        self.visited = '0'
       
     def execute_cb(self, goal):
 
@@ -40,41 +51,38 @@ class RandomMovementServer(object):
         success = True
         
 
-	#Choosing the desired coordinates
-	txt=open("/home/alex/Desktop/coords.txt","r")
-	limits=ast.literal_eval(txt.read())
-
-	position = rospy.wait_for_message("/amcl_pose", PoseWithCovarianceStamped, timeout=0.5)
-
-	x1=position.pose.pose.position.x
-	y1=position.pose.pose.position.y
-
-	if limits[0][0][0] <= x1 <= limits[0][0][1] and limits[0][1][0] <= y1 <= limits[0][1][1]:
-	    visited='0'
-	if limits[1][0][0] <= x1 <= limits[1][0][1] and limits[1][1][0] <= y1 <= limits[1][1][1]:
-	    visited='1'
-	if limits[2][0][0] <= x1 <= limits[2][0][1] and limits[3][1][0] <= y1 <= limits[2][1][1]:
-	    visited='2'
-	if limits[3][0][0] <= x1 <= limits[3][0][1] and limits[3][1][0] <= y1 <= limits[3][1][1]:
-	    visited='3'
-	
-	print(visited)
-
-	a=0
-	for i in goal.constants:
+        a=0
+        for i in goal.constants:
 	    a+=1
-	    if i==visited:
-		break
-	if a==len(goal.constants):
-	    a=0
+            if i==self.visited:
+            	break
 
-	room_limits=limits[ast.literal_eval(goal.constants[a])]
+        if a==len(goal.constants):
+            a=0
+
+        room_limits=self.limits[ast.literal_eval(goal.constants[a])]
 	
         x=round(random.uniform(room_limits[0][0],room_limits[0][1]),4)
         y=round(random.uniform(room_limits[1][0],room_limits[1][1]),4)
         result=str(x)+";"+str(y)+";0;0;0;0;1"
+
+        position = rospy.wait_for_message("/amcl_pose", PoseWithCovarianceStamped, timeout=0.5)
+
+        x1=position.pose.pose.position.x
+        y1=position.pose.pose.position.y
+ 
+
+        if self.limits[0][0][0] <= x1 <= self.limits[0][0][1] and self.limits[0][1][0] <= y1 <= self.limits[0][1][1]:
+            self.visited='0'
+        elif self.limits[1][0][0] <= x1 <= self.limits[1][0][1] and self.limits[1][1][0] <= y1 <= self.limits[1][1][1]:
+            self.visited='1'
+        elif self.limits[2][0][0] <= x1 <= self.limits[2][0][1] and self.limits[3][1][0] <= y1 <= self.limits[2][1][1]:
+            self.visited='2'
+        elif self.limits[3][0][0] <= x1 <= self.limits[3][0][1] and self.limits[3][1][0] <= y1 <= self.limits[3][1][1]:
+            self.visited='3'
+
 	    
-        print(result)
+
 	
 	if self._as.is_preempt_requested():
             rospy.loginfo('%s: Preempted' % self._action_name)
@@ -90,6 +98,7 @@ class RandomMovementServer(object):
             self._as.set_aborted()
             self._result.result = "Nao consigo andar. Um obstaculo!"
             rospy.loginfo('%s: Failed' % self._action_name)
+	
         
 	r.sleep()
         
